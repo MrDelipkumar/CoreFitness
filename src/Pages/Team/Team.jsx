e0794bfd38cb9d9bed6c89072dd60ad1c8f6229d
MrDelipkumar <Delipkumar2002@outlook.com>
2024-06-07 20:17:32 +0530

    Initial Commit

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AiOutlineTwitter , AiOutlineInstagram , AiOutlineWhatsApp } from "react-icons/ai";
 import crossfit from '../../assets/img-1.jpg'
-import weightlifting from '../../assets/img-2.webp'
+import weightlifting from '../../assets/img-2.jpg'
 import Fitness from '../../assets/img-3.jpg'
 import bodybuilding from '../../assets/img-4.webp'
 function Team() {
